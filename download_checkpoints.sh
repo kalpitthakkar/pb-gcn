1e4e7cd5ae414e4748897f5d54cf56c698cd3ec8
kalpitthakkar <kthakkar1605@gmail.com>
2019-06-09 22:49:51 -0400

    Add checkpoints download script

---
+++ download_checkpoints.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p weights
+
+wget -c -O weights/crossview_weights.pth https://www.dropbox.com/s/dlhyo4bsyquvk55/crossview_weights.pth?dl=0
+
+wget -c -O weights/crosssubject_weights.pth https://www.dropbox.com/s/3n2rxosp78vdtj1/crosssubject_weights.pth?dl=0
